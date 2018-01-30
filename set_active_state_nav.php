c44ba861688f23d47f43825d515969c3d76c7739
Nick Mole <nmole065@g.rwu.edu>
2018-01-30 13:17:39 -0500

    first commit

---
+++ set_active_state_nav.php
@@ -0,0 +1,24 @@
+<?php
+ /**
+ * Plugin Name: Set Active State Nav
+ * Description: Create extra user fields
+ * Version: 1.0x
+ * Author: Nick Mole
+ * Text Domain: sasn-set-active-state-nav
+ */
+
+//Paths for fields
+//require_once plugin_dir_path(__FILE__) . 'src/Helpers.php';
+require_once plugin_dir_path(__FILE__) . 'src/SetActiveStateNav.php';
+
+
+use Mole\SASN;
+use Mole\SASN\SetActiveStateNav;
+//use Mole\AUF\Helpers;
+
+new SetActiveStateNav();
+
+
+
+
+?>
---
+++ src/SetActiveStateNav.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mole\SASN;
+
+//use Mole\AUF\Helpers;
+
+class SetActiveStateNav {
+
+  //Contructor
+  public function __construct()
+  { 
+    
+
+  }
+
+}
